8582ca03a712c6e1e98958d94a5062a85cde8de1
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-01-24 15:28:45 -0300

    fix

--- src/models/creational/factory-method/specific/windowsDialog.ts
+++ src/models/creational/factory-method/specific/windowsDialog.ts
@@ -2,7 +2,7 @@ import Dialog from './dialog';
 import windowsButton from './windowsButton';
 
 export default class WindowDialog extends Dialog{
-  name: 'Windows Dialog';
+  name:string =  'Windows Dialog';
   title:string = 'THIS IS A Windows Dialog';
   icon:string = 'mdi-windows';
 
