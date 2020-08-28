8d5f97cadb324c94476b72e97312d6e0c4b4bea6
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-08-28 18:56:12 -0300

    typed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import App from './app'
 
 import Controllers from './controllers'
 
-const port = Number.parseInt(process.env.PORT)  || 3000;
+const port:number = Number.parseInt(process.env.PORT)  || 3000;
 
 const app = new App({ 
     port, 
